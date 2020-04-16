f804c3acaa27de202a21da94bd49ac3b3a930775
wuyongxugogogo <60922797+wuyongxugogogo@users.noreply.github.com>
2020-04-16 10:41:59 +0800

    第六题 Z型字符串

---
+++ leetcode/6.js
@@ -0,0 +1,38 @@
+// 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+
+// 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+
+// L   C   I   R
+// E T O E S I I G
+// E   D   H   N
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"
+// 不用输出Z字形 没有空格
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if(numRows == 1)
+        return s;
+
+    const len = Math.min(s.length, numRows);
+    const rows = [];
+    for(let i = 0; i< len; i++) rows[i] = "";
+    let loc = 0;
+    let down = false;
+
+    for(const c of s) {
+        rows[loc] += c;
+        if(loc == 0 || loc == numRows - 1)
+            down = !down;
+        loc += down ? 1 : -1;
+    }
+
+    let ans = "";
+    for(const row of rows) {
+        ans += row;
+    }
+    return ans;
+};
+console.log(convert("asdvbfasdc",3))
