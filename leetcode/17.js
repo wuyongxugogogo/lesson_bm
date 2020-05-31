af539d38b725ec641d0f26b4ce5c186a90fb3acf
wuyongxugogogo <474643974@qq.com>
2020-05-31 14:48:16 +0800

    第17题

---
+++ leetcode/17.js
@@ -0,0 +1,35 @@
+// 电话号码的字母组合
+// 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+var letterCombinations = function (digits) {
+    let result = [];
+    const hashMap = {
+        2: 'abc',
+        3: 'def',
+        4: 'ghi',
+        5: 'jkl',
+        6: 'mno',
+        7: 'pqrs',
+        8: 'tuv',
+        9: 'wxyz'
+    }
+    for (let code of digits) {
+        let words = hashMap[code];
+        if (result.length > 0) {
+            let arr = [];
+            for (let first of result) {
+                for (let word of words) {
+                    arr.push(first + word)
+                }
+            }
+            result = arr
+        } else {
+            result.push(...words)
+        }
+    }
+    console.log(result)
+    return result
+};
+letterCombinations('489')
