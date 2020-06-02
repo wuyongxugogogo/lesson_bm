4e39616fa0f76c9651f40f697d975605dfe64680
wuyongxugogogo <474643974@qq.com>
2020-06-02 21:34:42 +0800

    beer

--- react/do-beer/README.md
+++ react/do-beer/README.md
@@ -3,4 +3,15 @@ beer  搜索  -> 列表 -> 点进去 创建路由
 
 3个路由设计 beer 这三个路由
 / RESTFUL
-搜索根列表现实合二为一？ http://www.cesheng.com/search/:searchTerm?a=q&b=2&c=3
+搜索根列表现实合二为一？ http://www.cesheng.com/search/:searchTerm?a=q&b=2&c=3  /search/all  /search/8  params
+详情页  /beer/:beerId   detail    details/item?id=
+语义化设计URL  /post/:postId
+
+1. 把应用架构搭好，路由
+2. 组件化思维
+3. list列表的编写流程
+4. router  this.props.match.params
+5.  / -> Main.js -> loadBeers -> loading -> list
+6. / -> /search/:searchTerm 路由, -> rule -> 显示
+componentDidMount
+
--- react/do-beer/src/components/Search.js
+++ react/do-beer/src/components/Search.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import PropTypes from 'prop-types';
 
-class Search extends React.Component{
-    render(){
-        return(
-            <div>Search</div>
+// StatefullComponent  StatelessComponent
+// Main.js -> 
+class Search extends React.Component {
+
+    static contextTypes = { // 静态属性
+        router: PropTypes.object.isRequired
+    }
+    // public 属性 react 不能去dom querySelector 
+    searchRef = React.createRef();
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const searchTerm = this.searchRef.current.value;
+        console.log(this.context);
+        // 1. 用户在输入框中的值 ref 
+        // console.log(searchTerm, '-------------');
+        // console.log(this.context, '+++++++++++');
+        this.context.router.history.push(`/search/${searchTerm}`);
+        // window.history.push(`/search/${searchTerm}`);
+    }
+
+    render() {
+        console.log(this.searchRef);
+
+        return (
+            // 表单  
+            <div className="search">
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" ref={this.searchRef}
+                        placeholder="Hoppy, Malt, Angry, New..." />
+                    <input type="submit" value="Search" />
+                </form>
+            </div>
         )
     }
 }
