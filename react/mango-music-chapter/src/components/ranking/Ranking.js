97bd34c47e529e398b14e450c7901a048c7aaec3
wuyongxugogogo <474643974@qq.com>
2020-06-05 22:10:16 +0800

    mango-music

---
+++ react/mango-music-chapter/src/components/ranking/Ranking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ranking.styl';
+
+class Ranking extends React.Component{
+    render(){
+        return(
+            <div>Ranking</div>
+        )
+    }
+}
+
+export default Ranking;
---
+++ react/mango-music-chapter/src/components/search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search.styl';
+
+class Search extends React.Component{
+    render(){
+        return(
+            <div>Search</div>
+        )
+    }
+}
+
+export default Search;
