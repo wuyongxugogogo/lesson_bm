df0a355bae32f7e9be28efaca41c126a35f7308a
wuyongxugogogo <474643974@qq.com>
2020-07-15 12:02:16 +0800

    屏幕像素比

---
+++ react/rem/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <h2>LOGO</h2>
+        <input type="text" placeholder="search"/>
+        <button>登录</button>
+      </div>
+      <div className="app">
+      </div>
+    </div>
+  );
+}
+
+export default App;
