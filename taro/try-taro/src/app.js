0d40857be1687fc000cf811ef6f0b43255c9a725
wuyongxugogogo <474643974@qq.com>
2020-08-07 11:51:33 +0800

    .

---
+++ taro/try-taro/src/app.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './app.scss'
+
+class App extends React.Component {
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // this.props.children 是将要会渲染的页面
+  render () {
+    return this.props.children
+  }
+}
+
+export default App
