3b7412a057fd1b2341b47ac08e2c729b42dc9793
wuyongxugogogo <474643974@qq.com>
2020-08-17 11:56:07 +0800

    webpack

--- js/babel/src/App.jsx
+++ js/babel/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import styles from './style.css';
 
 export default function App(){
     return(
-        <div>
+        <div className={styles['swiper-container']}>
+            <img src="/ip6x2.jpg" alt="图片加载出错"/>
             this is app
         </div>
     )
--- js/babel/webpack.config.js
+++ js/babel/webpack.config.js
@@ -1,21 +1,47 @@
+// 自动
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const CopyPlugin = require('copy-webpack-plugin')
+
 const config = {
     entry: './src/index.js',
     resolve: {
         extensions: ['.js', '.jsx']
     },
     devServer: {
-        contentBase: './dist',
+        contentBase: './public',
         hot: true
     },
     module: {
         rules: [
             {
-                test: /(.js|.jsx$)/,
+                test: /(.js|.jsx)$/,
                 use: ['babel-loader'],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        modules: 'local',
+                        localIdentName: '[hash:base64:5]'
+                    }
+                }]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new BundleAnalyzerPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: './public/ip6x2.jpg', to: './' }
+            ],
+        })
+    ]
 }
-
 module.exports = config
