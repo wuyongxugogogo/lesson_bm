fdf65652f925f701a8d89226921a99487bcfb357
wuyongxu <474643974@qq.com>
2021-01-28 17:26:22 +0800

    webpack配置学习

---
+++ webpackLearn/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  // JavaScript 执行入口文件
+  entry: './main.js',
+  output: {
+    // 把所有依赖的模块合并输出到一个 bundle.js 文件
+    filename: 'bundle.js',
+    // 输出文件都放到 dist 目录下
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        // 用正则去匹配要用该 loader 转换的css文件
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
+};
